fa0efc4c78b00975ea817abee34ffc0b700e1814
Jason <jcoro045@ucr.edu>
2023-08-07 01:26:37 -0700

    closes #9

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 import SigninButton from "@/components/SigninButton";
-import { useSession } from "next-auth/react";
+import { useSession, signIn } from "next-auth/react";
+import Image from "next/image";
 import { useEffect } from "react";
+import User from "@/../public/user.svg";
 
 export default function Home() {
   const session = useSession();
@@ -13,7 +15,26 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center py-12 bg-white">
-      <SigninButton />
+      <div className="flex flex-row font-medium w-2/3 justify-between text-xl text-slate-500 items-center">
+        QuizAjej
+        <div className="w-24">
+          {session.status === "authenticated" ? (
+            <div className="bg-slate-300 px-1 py-2.5 gap-1 text-xs text-black/50 rounded-xl justify-center items-center flex">
+              <Image
+                src={User}
+                width={20}
+                height={20}
+                alt=""
+                className="stroke-slate-500"
+              />
+              {/*@ts-ignore*/}
+              {session.data.user.username}
+            </div>
+          ) : (
+            <SigninButton loading={session.status == "loading"} />
+          )}
+        </div>
+      </div>
     </main>
   );
 }
--- src/components/SigninButton.tsx
+++ src/components/SigninButton.tsx
@@ -1,12 +1,19 @@
 import { signIn } from "next-auth/react";
 
-function SigninButton() {
+type ButtonProps = {
+  loading: boolean;
+};
+function SigninButton({ loading }: ButtonProps) {
   return (
     <button
       onClick={() => signIn("credentials")}
       className="bg-slate-300 px-4 py-2.5 hover:bg-slate-200 transition ease-in-out duration-300 delay-50 w-full focus:outline-none rounded-lg text-xs font-bold flex justify-center gap-4 text-black/50"
     >
-      "Sign in"
+      {loading ? (
+        <div className="text-slate-400 font-semibold">Loading...</div>
+      ) : (
+        "Sign in"
+      )}
     </button>
   );
 }
