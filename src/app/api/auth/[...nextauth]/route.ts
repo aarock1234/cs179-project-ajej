d3312262faf34c7018a145301f5e1b8df36666a7
aarock1234 <arockplaysmc@gmail.com>
2023-08-07 01:31:11 -0700

    closes #7

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,48 @@
+import NextAuth, { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { prisma } from "@/prisma";
+
+export const authOptions: AuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async jwt({ token, user, account, profile }) {
+      if (user) {
+        return { ...token, user, profile };
+      }
+      return token;
+    },
+
+    async session({ session, token, user }) {
+      return { ...session, ...token };
+    },
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials, req) => {
+        if (!credentials?.username) return null;
+
+        const user = await prisma.user.findUnique({
+          where: { username: credentials?.username },
+        });
+        const isValid = user?.password === credentials?.password;
+
+        if (user && isValid) {
+          return { id: user.id.toString(), username: user.username };
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
