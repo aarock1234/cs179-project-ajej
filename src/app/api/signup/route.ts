1c00296c5ddcefbf37bb9eb96b614321e511482b
aarock1234 <arockplaysmc@gmail.com>
2023-08-14 11:51:58 -0700

    bruh

---
+++ src/app/api/signup/route.ts
@@ -0,0 +1,34 @@
+import { prisma } from "@/prisma";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import { authOptions } from "../auth/[...nextauth]/route";
+
+const schema = z.object({
+  username: z.string(),
+  password: z.string(),
+});
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  const body = schema.safeParse(await req.json());
+  if (!body.success) return NextResponse.json(body.error);
+
+  const { username, password } = body.data;
+
+  if (await prisma.user.findUnique({ where: { username } })) {
+    return NextResponse.json(
+      {
+        error: "Username already exists",
+      },
+      { status: 400 }
+    );
+  }
+
+  const user = await prisma.user.create({
+    data: {
+      username,
+      password,
+    },
+  });
+
+  return NextResponse.json(user, { status: 201 });
+}
