6d2bc79018e4da1a189e230e09d4baf5e78396ee
aarock1234 <arockplaysmc@gmail.com>
2023-08-30 13:02:45 -0700

    quiz results on user page

--- src/app/api/user/[username]/route.ts
+++ src/app/api/user/[username]/route.ts
@@ -5,36 +5,46 @@ import { z } from 'zod';
 const prisma = new PrismaClient();
 
 const schema = z.object({
-  username: z.string(),
+	username: z.string(),
 });
 
 export async function POST(req: NextRequest) {
-  try {
-    const body = schema.safeParse(await req.json());
-    if (!body.success) return NextResponse.json(body.error);
+	try {
+		const body = schema.safeParse(await req.json());
+		if (!body.success) return NextResponse.json(body.error);
 
-    const { username } = body.data;
+		const { username } = body.data;
 
-    const user = await prisma.user.findUnique({
-      where: { username },
-      include: {
-        quizzes: {
-          include: {
-            likes: true,
-          },
-        },
-        followers: true,
-        following: true,
-      },
-    });
-    if (!user) {
-      return NextResponse.json({ error: 'User not found' }, { status: 404 });
-    }
-    return NextResponse.json(user);
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json({ error: 'Failed to retrieve user' }, { status: 500 });
-  } finally {
-    await prisma.$disconnect();
-  }
+		const user = await prisma.user.findUnique({
+			where: { username },
+			include: {
+				quizzes: {
+					include: {
+						likes: true,
+					},
+				},
+				results: {
+					include: {
+						quiz: {
+                            include: {
+                                questions: true,
+                                likes: true,
+                            }
+                        },
+					},
+				},
+				followers: true,
+				following: true,
+			},
+		});
+		if (!user) {
+			return NextResponse.json({ error: 'User not found' }, { status: 404 });
+		}
+		return NextResponse.json(user);
+	} catch (error) {
+		console.error(error);
+		return NextResponse.json({ error: 'Failed to retrieve user' }, { status: 500 });
+	} finally {
+		await prisma.$disconnect();
+	}
 }
